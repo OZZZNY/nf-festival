892dd40aafb1a7a58a34e24be820f8e2fc1af7dc
Ozny <SB.Hub@hotmail.com>
2026-06-04 15:28:44 +0900

    Create script.js

---
+++ script.js
@@ -0,0 +1,82 @@
+window.onload = async function(){
+
+const savedId =
+localStorage.getItem("ticketId");
+
+if(savedId){
+
+checkStatus(savedId);
+
+}
+
+}
+
+async function reserve(){
+
+const name =
+document.getElementById("name").value;
+
+const keyword =
+document.getElementById("keyword").value;
+
+const res = await fetch("GASのURL",{
+
+method:"POST",
+
+body:JSON.stringify({
+name:name,
+keyword:keyword
+})
+
+});
+
+const data = await res.json();
+
+localStorage.setItem("ticketId", data.id);
+
+document.getElementById("formArea").style.display = "none";
+
+checkStatus(data.id);
+
+}
+
+async function checkStatus(id){
+
+const res = await fetch(
+"GASのURL?mode=status&id=" + id
+);
+
+const data = await res.json();
+
+let text = "";
+
+if(data.status === "WAITING"){
+
+text = `
+<h2>待機中</h2>
+<p>整理番号：${id}</p>
+<p>現在待機：${data.waiting}組</p>
+`;
+
+}
+
+if(data.status === "CALLED"){
+
+text = `
+<h2>お呼び出し中です！</h2>
+<p>整理番号：${id}</p>
+`;
+
+}
+
+if(data.status === "DONE"){
+
+text = `
+<h2>ご案内済みです</h2>
+`;
+
+}
+
+document.getElementById("result").innerHTML = text;
+
+}
