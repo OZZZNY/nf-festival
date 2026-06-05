9d8cdbe08156a857c01ae73983d2a594fc692e5f
Ozny <SB.Hub@hotmail.com>
2026-06-06 08:37:29 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,21 @@
-alert("読み込み成功");
-
 async function reserve(){
 
 alert("reserve開始");
+
+const res = await fetch(
+"https://script.google.com/macros/s/あなたのURL/exec",
+{
+method:"POST",
+body:JSON.stringify({
+name:"test",
+keyword:"test"
+})
+});
+
+alert("fetch成功");
+
+const text = await res.text();
+
+alert(text);
 
 }
