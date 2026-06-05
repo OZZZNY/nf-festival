df2ac1295596720570d4295263c6793cebbb6c51
Ozny <SB.Hub@hotmail.com>
2026-06-06 08:39:30 +0900

    Update script.js

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ async function reserve(){
 alert("reserve開始");
 
 const res = await fetch(
-"https://script.google.com/macros/s/あなたのURL/exec",
+"https://script.google.com/macros/s/AKfycby2GYeUMEk5QVjW3WpctBAjDi2yNshmFFypHePjeGAKtLx401EoUBYLqwjq4F3_xjiE7Q/exec",
 {
 method:"POST",
 body:JSON.stringify({
